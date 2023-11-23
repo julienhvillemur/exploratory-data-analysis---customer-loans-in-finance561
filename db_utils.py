30488856cee9b3a1e3527983e3fb0f00b07cb5a2
julienhvillemur <julienhvillemur@gmail.com>
2023-11-23 21:06:58 +0000

    This enables the csv file from the local machine to be loaded in as a dataframe.

--- db_utils.py
+++ db_utils.py
@@ -9,6 +9,7 @@ from sqlalchemy import create_engine
 
 import pandas as pd
 
+
 # Load the credentials yaml file
 def retrieve():
     with open('credentials.yaml', 'r') as file:
@@ -42,10 +43,16 @@ class RDSDatabaseConnector():
             global loan_payments
             loan_payments = pd.read_sql_table('loan_payments', engine)
 
-    # Save table as CSV file
+    # Save table as CSV file to local machine
     def save_file(self):
         loan_payments.to_csv('../../file_saves/loan_payments.csv', index=False)
 
+
+# Load CSV file as dataframe from local machine
+def open_table():
+    with open('../../file_saves/loan_payments.csv', 'r') as file:
+        saved_table = pd.read_csv(file)
+    return saved_table
 
 
 #TEST. DELETE ONCE COMPLETE.
@@ -53,6 +60,6 @@ credentials = retrieve()
 
 call = RDSDatabaseConnector(credentials)
 
-call.save_file()
+open_table().shape
 
 # %%
